a8b773f3a137a31ef703da1e4085beca5a4ede6c
Andrei1808 <kaliarovich93@gmail.com>
2022-10-11 00:05:45 +0300

    marathon

---
+++ script.js
@@ -0,0 +1,17 @@
+const slides = document.querySelectorAll('.slide');
+
+for (let slide of slides) {
+    slide.addEventListener('click', () => {
+        clearActiveClasses();
+
+        slide.classList.add('active');
+    } )
+}
+
+
+let clearActiveClasses = () => {
+    for (let slide of slides) {
+        slide.classList.remove('active');
+    }
+}
+
